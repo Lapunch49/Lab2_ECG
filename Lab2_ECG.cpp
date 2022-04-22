a03781b7cf1301950a0ef7c2c47d841d32efc308
Lapunch49 <bulatovach@gmail.com>
2022-04-22 09:44:37 +0300

    lesson 8

--- Lab2_ECG.cpp
+++ Lab2_ECG.cpp
@@ -36,10 +36,10 @@ void RenderSceneCB()
     Scale += 0.002f;
 
     Matrix4f World;
-    World.m[0][0] = cosf(Scale); World.m[0][1] = -sinf(Scale); World.m[0][2] = 0.0f;         World.m[0][3] = 0.0f;
-    World.m[1][0] = sinf(Scale); World.m[1][1] = cosf(Scale);  World.m[1][2] = -sinf(Scale); World.m[1][3] = 0.0f;
-    World.m[2][0] = 0.0f;        World.m[2][1] = sinf(Scale);  World.m[2][2] = cosf(Scale);  World.m[2][3] = 0.0f;
-    World.m[3][0] = 0.0f;        World.m[3][1] = 0.0f;         World.m[3][2] = 0.0f;         World.m[3][3] = 1.0f;
+    World.m[0][0] = 0.5*sinf(Scale); World.m[0][1] = 0.0f;        World.m[0][2] = 0.0f;        World.m[0][3] = 0.0f;
+    World.m[1][0] = 0.0f;        World.m[1][1] = 0.5*cosf(Scale); World.m[1][2] = 0.0f;        World.m[1][3] = 0.0f;
+    World.m[2][0] = 0.0f;        World.m[2][1] = 0.0f;        World.m[2][2] = sinf(Scale); World.m[2][3] = 0.0f;
+    World.m[3][0] = 0.0f;        World.m[3][1] = 0.0f;        World.m[3][2] = 0.0f;        World.m[3][3] = 1.0f;
 
     glUniformMatrix4fv(gWorldLocation, 1, GL_TRUE, &World.m[0][0]);
 
